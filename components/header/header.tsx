f8924ff97d4b8a2ff203a5748a59fd14e15c0f17
Farouk-ayo <mustaphafarouk41@gmail.com>
2025-10-23 12:39:22 +0100

    feat: responsive header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,10 +1,3 @@
-import {
-  BottomLeftIcon,
-  BottomRightIcon,
-  TopLeftIcon,
-  TopRightIcon,
-} from "../icons";
-
 interface HeaderProps {
   title: string;
   description: React.ReactNode;
@@ -25,18 +18,6 @@ export default function Header({ title, description }: HeaderProps) {
       <div className="absolute inset-0  bg-primary " />
 
       <div className="relative w-full md:w-[90%] mx-auto p-5 z-20 md:p-10 flex flex-col gap-5 text-center">
-        <div className="absolute top-0 left-0 ">
-          <TopLeftIcon />
-        </div>
-        <div className="absolute top-0 right-0 ">
-          <TopRightIcon />{" "}
-        </div>
-        <div className="absolute bottom-0 left-0 ">
-          <BottomLeftIcon />{" "}
-        </div>
-        <div className="absolute bottom-0 right-0 ">
-          <BottomRightIcon />{" "}
-        </div>
         <h1 className="text-3xl md:text-5xl lg:text-7xl font-bold text-secondary">
           {title}
         </h1>
